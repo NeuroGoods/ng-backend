8f603804f0774f4046b311d70f9884a43adbc5f7
JoseRD149 <joseromerodiaz89@gmail.com>
2025-03-20 15:37:51 +0100

    Feat: add ReviewController with CRUD operations for reviews

---
+++ app/Http/Controllers/api/ReviewController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Review;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReviewController extends Controller
+{
+   
+    public function index()
+    {
+        $review = PReview::all();
+        return response()->json($review);
+    }
+
+    
+    public function create()
+    {
+        
+    }
+
+ 
+     
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+         
+            'user_id' => 'required'
+            'product_id' => 'required',
+            'comment' => 'required',
+            'rating' => 'required',
+            
+             
+        ]);
+
+        $review = review::create($validatedData);
+        return response()->json($review, 201);
+    }
+
+   
+    public function show(string $id)
+    {
+        $review = Review::find($id);
+        return response()->json($review);
+    }
+
+    
+    public function edit(string $id)
+    {
+        
+    }s
+
+ 
+     
+    public function update(Request $request, string $id)
+    {
+        $review = Review::find($id);
+
+        if (!$review) {
+            return response()->json(['message' => 'review not found'], 404);
+        }
+
+        $validated = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'category_id' => 'required',
+        ]);
+
+        $review->update(array_filter($validated));
+        return response()->json($review, 200);
+    }
+
+    
+    public function destroy(string $id)
+    {
+        $review = Review::find($id);
+
+        if (!$review) {
+            return response()->json(['message' => 'Review not found'], 404);
+        }
+
+        $review->delete();
+        return response()->json(['message' => 'Review deleted'], 200);
+    }
+}
