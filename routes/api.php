c5346f0ecd1805b2f4eadbb0fad4ccbde8f987bb
issamchlf <issamchellaf734@gmail.com>
2025-03-20 16:15:32 +0100

    Fix: update API routes to correct controller references and improve organization

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ReviewController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
@@ -24,20 +21,20 @@ Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::put('/categories/{id}', [CategoryController::class, 'update']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-Route::get('/orders', [CategoryOrder::class, 'index']);
-Route::post('/orders', [CategoryOrder::class, 'store']);
-Route::get('/orders/{id}', [CategoryOrder::class, 'show']);
-Route::put('/orders/{id}', [CategoryOrder::class, 'update']);
-Route::delete('/orders/{id}', [CategoryOrder::class, 'destroy']);
+Route::get('/orders', [OrderController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/orders/{id}', [OrderController::class, 'show']);
+Route::put('/orders/{id}', [OrderController::class, 'update']);
+Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
 
-Route::get('/reviews', [CategoryReview::class, 'index']);
-Route::post('/reviews', [CategoryReview::class, 'store']);
-Route::get('/reviews/{id}', [CategoryReview::class, 'show']);
-Route::put('/reviews/{id}', [CategoryReview::class, 'update']);
-Route::delete('/reviews/{id}', [CategoryReview::class, 'destroy']);
+Route::get('/reviews', [ReviewController::class, 'index']);
+Route::post('/reviews', [ReviewController::class, 'store']);
+Route::get('/reviews/{id}', [ReviewController::class, 'show']);
+Route::put('/reviews/{id}', [ReviewController::class, 'update']);
+Route::delete('/reviews/{id}', [ReviewController::class, 'destroy']);
 
-Route::get('/users', [CategoryUser::class, 'index']);
-Route::post('/users', [CategoryUser::class, 'store']);
-Route::get('/users/{id}', [CategoryUser::class, 'show']);
-Route::put('/users/{id}', [CategoryUser::class, 'update']);
-Route::delete('/users/{id}', [CategoryUser::class, 'destroy']);
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
