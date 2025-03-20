37eb429e62c65130d5d55dabbc296bf6e42ed451
issamchlf <issamchellaf734@gmail.com>
2025-03-20 21:06:21 +0100

    test: add feature tests for User model attributes

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+
+class UserTest extends TestCase
+{
+    
+    use RefreshDatabase;
+
+    public function test_fillable_attributes()
+    {
+        $user = new User();
+        $this->assertEquals(['name', 'email', 'password'], $user->getFillable());
+    }
+
+    public function test_hidden_attributes()
+    {
+        $user = new User();
+        $this->assertEquals(['password', 'remember_token'], $user->getHidden());
+    }
+
+    public function test_casts_attributes()
+    {
+        $user = new User();
+        $expectedCasts = [
+            'id' => 'int',
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+        $this->assertSame($expectedCasts, $user->getCasts());
+    }
+}
